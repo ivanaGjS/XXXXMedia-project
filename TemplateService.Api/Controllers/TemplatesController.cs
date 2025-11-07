254c8f79b7cb0bc7256dd09bea079f127dff1f65
ivanaGjS <ivana.gjorgjievska@gmail.com>
2025-11-07 11:28:01 +0100

    Add files via upload

---
+++ TemplateService.Api/Controllers/TemplatesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XXXXMedia.Shared.Persistence.Entities;
+using XXXXMedia.Shared.Persistence.Repositories.Interfaces;
+
+namespace TemplateService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TemplatesController : ControllerBase
+{
+    private readonly IRepository<Template> _repo;
+    public TemplatesController(IRepository<Template> repo) { _repo = repo; }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Template template)
+    {
+        var saved = await _repo.AddAsync(template);
+        return Ok(new { id = saved.Id });
+    }
+}
---
+++ TemplateService.Api/Program.cs
@@ -0,0 +1,15 @@
+﻿using XXXXMedia.Shared.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSharedDependencies(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
